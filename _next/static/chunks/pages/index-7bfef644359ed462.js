4cc9bdb9f9cdd4dda12a7975a92f31f2867e8d74
s1f102003425 <s1f102003425@users.noreply.github.com>
2023-06-17 12:51:58 +0000

    deploy: 2e5a8dfd2479afe842b883f70fd2e08c88874242

--- _next/static/chunks/pages/index-7bfef644359ed462.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(85)}])},85:function(n,u,_){"use strict";_.r(u);let t=()=>{};u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
