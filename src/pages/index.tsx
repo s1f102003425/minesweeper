ac6b2cf46341240bd0633452736ac52ae61fad7f
root <root@mousenote1>
2023-06-19 20:14:10 +0900

    boardの表現作成中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ const Home = () => {
   const bombCount = 10;
   // 0 -> ボム無し
   // 1 -> ボム有り
-  const [bombMap, setBombMap] = useState([
+  const [bombMap, setBombMap] = useState<(0 | 1)[][]>([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -54,29 +54,36 @@ const Home = () => {
   const board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, 0, -1, -1, -1],
+    [-1, -1, -1, -1, -1, 1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, 2, -1, -1, -1],
+    [-1, -1, -1, -1, -1, 3, -1, -1, -1],
+    [-1, 9, -1, -1, -1, 4, -1, -1, -1],
+    [-1, 10, -1, -1, -1, 5, 8, -1, -1],
+    [-1, 11, -1, -1, -1, 6, 7, -1, -1],
   ];
+  const onClick = (x: number, y: number) => {
+    const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+    newUserInputs[y][x] = +1;
+    console.log(`${x}-${y}`);
+    setUserInputs(newUserInputs);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         <div className={styles['play-board']}>
-          {}
           {board.map((row, y) =>
             row.map((display, x) => (
               <div
                 className={styles.icon}
-                key={'${x}-${y}'}
+                key={`${x}-${y}`}
+                onClick={() => onClick(x, y)}
                 style={{
                   backgroundPosition: 30 - 30 * display,
-                  border: display === -1 ? 5 : 1,
-                  borderStyle: display === -1 ? 'outset' : 'solid',
-                  borderColor: '#f9f9f9',
+                  border:
+                    display === -1 || display === 9 || display === 10
+                      ? '5px #f9f9f9 outset'
+                      : '5px #f9f9f9 solid',
                 }}
               />
             ))
