d3a6fbdf1ea95576f230ac0370b90d10b75d0ce6
root <root@mousenote1>
2023-06-19 23:54:03 +0900

    初手でボムを配置する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const normalBoard: 0[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
   const directions = [
     [-1, -1],
     [-1, 0],
@@ -16,31 +27,11 @@ const Home = () => {
   // 1 -> 左クリック
   // 2 -> はてな
   // 3 -> 旗
-  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>(normalBoard);
   const bombCount = 10;
   // 0 -> ボム無し
   // 1 -> ボム有り
-  const [bombMap, setBombMap] = useState<(0 | 1)[][]>([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+  const [bombMap, setBombMap] = useState<(0 | 1)[][]>(normalBoard);
   const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
   const isFailure = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[x][y] === 1)
@@ -54,20 +45,33 @@ const Home = () => {
   const board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, 0, -1, -1, -1],
-    [-1, -1, -1, -1, -1, 1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, 2, -1, -1, -1],
-    [-1, -1, -1, -1, -1, 3, -1, -1, -1],
-    [-1, 9, -1, -1, -1, 4, -1, -1, -1],
-    [-1, 10, -1, -1, -1, 5, 8, -1, -1],
-    [-1, 11, -1, -1, -1, 6, 7, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
   ];
-  const onClick = (x: number, y: number) => {
+  const userClick = (x: number, y: number) => {
+    console.log(x, y);
     const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
-    newUserInputs[y][x] = +1;
-    console.log(`${x}-${y}`);
-    setUserInputs(newUserInputs);
+    const newBombMap: (0 | 1)[][] = JSON.parse(JSON.stringify(bombMap));
+    if (normalBoard === bombMap) {
+      let i = 1;
+      while (i < 11) {
+        const xBomb = Math.floor(Math.random() * 9);
+        const yBomb = Math.floor(Math.random() * 9);
+        if (newBombMap[yBomb][xBomb] === 0 && `${yBomb}-${xBomb}` !== `${y}-${x}`) {
+          newBombMap[yBomb][xBomb] = 1;
+          i += 1;
+        }
+        setBombMap(newBombMap);
+      }
+      console.table(newBombMap);
+    }
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -77,11 +81,11 @@ const Home = () => {
               <div
                 className={styles.cell}
                 key={`${x}-${y}`}
-                onClick={() => onClick(x, y)}
+                onClick={() => userClick(x, y)}
                 style={{
                   border:
                     display !== -1 && display !== 9 && display !== 10
-                      ? '2px solid #999'
+                      ? '1px solid #999'
                       : undefined,
                 }}
               >
@@ -93,8 +97,8 @@ const Home = () => {
                       display !== -1 && display !== 9 && display !== 10
                         ? '5px #d6d7d8  solid'
                         : '5px #f9f9f9 outset',
-                    width: display !== -1 && display !== 9 && display !== 10 ? 36 : 40,
-                    height: display !== -1 && display !== 9 && display !== 10 ? 36 : 40,
+                    width: display !== -1 && display !== 9 && display !== 10 ? 38 : 40,
+                    height: display !== -1 && display !== 9 && display !== 10 ? 38 : 40,
                   }}
                 />
               </div>
