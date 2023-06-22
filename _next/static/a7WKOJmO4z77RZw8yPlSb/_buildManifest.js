0c21b9983e5465e066b151ae5b5c07d1f10e2a76
s1f102003425 <s1f102003425@users.noreply.github.com>
2023-06-22 09:28:33 +0000

    deploy: 0c139b18eed1b23e61f542552db7d7a0a4fbda1f

--- _next/static/a7WKOJmO4z77RZw8yPlSb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ce4d9ef80dd28d55.css","static/chunks/pages/index-70e65630f1cd7369.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
